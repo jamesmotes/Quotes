6f81e1b9e5c93442174098e4df3cde660da5d1c9
jmotes <jmotes@tamu.edu>
2017-12-26 15:28:01 -0600

    Initial Commit

---
+++ Quotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quotes
+//
+//  Created by James Motes on 12/26/17.
+//  Copyright © 2017 JDM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
