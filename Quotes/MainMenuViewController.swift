6ac61e836f36102a6770d3a8d24cb604372c9b75
jmotes <jmotes@tamu.edu>
2018-01-22 20:52:28 -0600

    added transitions form main menu

--- Quotes/MainMenuViewController.swift
+++ Quotes/MainMenuViewController.swift
@@ -14,7 +14,7 @@ class MainMenuViewController: UIViewController, UICollectionViewDelegate, UIColl
     
     @IBOutlet weak var navItem: UINavigationItem!
     
-    let mainMenu = ["People", "Categories", "Favorites", "Mood", "Random", "Settings"]
+    let mainMenu = ["People", "Categories", "Mood", "Favorites", "Random", "Settings"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,13 +29,32 @@ class MainMenuViewController: UIViewController, UICollectionViewDelegate, UIColl
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MenuCell", for: indexPath) as! MainMenuCell
-        
+        cell.cellLabel.text = mainMenu[indexPath.item]
         return cell
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        switch indexPath.item {
+        case 0:
+            performSegue(withIdentifier: "menuToPeople", sender: nil)
+        case 1:
+            performSegue(withIdentifier: "menuToCategories", sender: nil)
+        case 2:
+            performSegue(withIdentifier: "menuToMood", sender: nil)
+        case 3:
+            performSegue(withIdentifier: "menuToFavorites", sender: nil)
+        case 4:
+            performSegue(withIdentifier: "menuToRandom", sender: nil)
+        case 5:
+            performSegue(withIdentifier: "menuToSettings", sender: nil)
+        default:
+            print("none selected")
+        }
     }
 
 }
