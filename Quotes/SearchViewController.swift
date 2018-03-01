208c9ccee92954cce75d5e831e5dafba648584ae
jmotes <jmotes@tamu.edu>
2018-03-01 17:30:08 -0600

    mid progress on search bar

--- Quotes/SearchViewController.swift
+++ Quotes/SearchViewController.swift
@@ -1,18 +1,36 @@
 //
 //  SearchViewController.swift
-//  Quotes
+//  
 //
-//  Created by James Motes on 2/27/18.
-//  Copyright © 2018 JDM. All rights reserved.
+//  Created by James Motes on 3/1/18.
 //
 
 import UIKit
+import Realm
+import RealmSwift
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    let realm = try! Realm()
+    
+    
+    var data : [String]!
+    
+    var filteredData: [String]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        let objects = Array(realm.objects(Quote.self))
+        for obj in objects {
+            data.append(obj.person)
+            data.append(contentsOf: obj.moods)
+            data.append(
+        }
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +39,35 @@ class SearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func backToView(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredData is the same as the original data
+        // When user has entered text into the search box
+        // Use the filter method to iterate over all items in the data array
+        // For each item, return true if the item should be included and false if the
+        // item should NOT be included
+        
+        
+        
+        
+        filteredData = searchText.isEmpty ? data : data.filter { (item: String) -> Bool in
+            // If dataItem matches the searchText, return true to include it
+            return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
+        
+        tableView.reloadData()
+    }
+    
 }
